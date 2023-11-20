5ac4f5a3378434801af99d24f38f74473db8437d
zyc <96008720+Yuchen114514@users.noreply.github.com>
2023-11-20 14:32:35 +0800

    Add files via upload

--- freeTime.py
+++ freeTime.py
@@ -4,9 +4,9 @@ import webbrowser
 from collections import defaultdict
 from datetime import datetime, timedelta
 from tkinter import *
-from tkinter import ttk
-from tkinter import font
-from tkinter import messagebox
+from tkinter import messagebox, Label, Entry, Button, Frame, Menu, ttk, font
+import tkinter as tk
+import os
 
 
 # 合并时间段
@@ -165,39 +165,102 @@ def gui_main_loop(data):
     window.mainloop()
 
 
-def token_input_window():
-    token_window = Tk()
-    token_window.title("Token 输入")
-    token_window.geometry("300x150")
+# 加载历史记录
+def load_history():
+    if os.path.exists("history.txt"):
+        with open("history.txt", "r") as file:
+            return file.read().splitlines()[-5:]  # 只保留最后5个条目
+    return []
 
-    token_label = Label(token_window, text="请输入Token:")
+
+# 保存历史记录
+def save_history(token):
+    history = load_history()
+    if token in history:
+        history.remove(token)  # 如果已存在，先移除
+    history.insert(0, token)  # 添加到列表开头
+
+    # 保存最多最后的 5 个条目（最新的 5 个）
+    with open("history.txt", "w") as file:
+        for item in history[:5]:
+            file.write(item + "\n")
+
+
+# Token 输入窗口
+def token_input_window():
+    token_window = tk.Tk()
+    token_window.title("Token 输入")
+    token_window.geometry("350x150")
+
+    history = load_history()
+
+    token_label = Label(token_window, text="请输入Token，右键查看历史记录")
     token_label.pack()
-    token_frame = Frame(token_window)  # 创建一个框架来包含输入框和粘贴按钮
+
+    token_frame = Frame(token_window)
     token_frame.pack()
 
-    token_var = StringVar(token_window)
+    token_var = tk.StringVar(token_window)
     token_entry = Entry(token_frame, textvariable=token_var)
-    token_entry.pack(side=LEFT)
+    token_entry.pack(side=tk.LEFT)
 
     def paste_from_clipboard():
         try:
-            # 从剪贴板获取文本并插入到输入框
             clipboard_text = token_window.clipboard_get()
             token_entry.insert(0, clipboard_text)
         except:
             messagebox.showerror("错误", "无法从剪贴板粘贴")
 
     paste_button = Button(token_frame, text="粘贴", command=paste_from_clipboard)
-    paste_button.pack(side=LEFT)
+    paste_button.pack(side=tk.LEFT)
+
+    def clear_history():
+        open("history.txt", "w").close()  # 清空文件
+        history.clear()  # 清空历史记录列表
+
+    def update_history(event=None):
+        global history
+        history = load_history()
+
+    token_entry.bind("<KeyRelease>", update_history)
+
+    def on_entry_click(event):
+        history_menu = tk.Menu(token_window, tearoff=0)
+        for item in history:
+            history_menu.add_command(
+                label=item, command=lambda value=item: token_var.set(value)
+            )
+        history_menu.add_separator()
+        history_menu.add_command(label="清除历史记录", command=clear_history)
+        history_menu.tk_popup(event.x_root, event.y_root)
+
+    token_entry.bind("<Button-3>", on_entry_click)  # 右键点击
 
     status_label = Label(token_window, text="")
     status_label.pack()
 
+    def has_consecutive_chars(s, threshold=4):
+        count = 1
+        for i in range(1, len(s)):
+            if s[i] == s[i - 1]:
+                count += 1
+                if count >= threshold:
+                    return True
+            else:
+                count = 1
+        return False
+
     def validate_token():
+        token = token_var.get()
+        if len(token) < 30:
+            status_label.config(text="Token 验证失败，Token 位数小于30，请重新输入。")
+            return
+        if has_consecutive_chars(token):
+            status_label.config(text="Token格式错误，请重新输入。")
+            return
+        save_history(token)
         status_label.config(text="验证中，请稍候...")
         token_window.update_idletasks()
-
-        token = token_var.get()
         try:
             response = requests.post(
                 url="https://jwglweixin.bupt.edu.cn/bjyddx/todayClassrooms?campusId=01",
@@ -210,7 +273,11 @@ def token_input_window():
             else:
                 status_label.config(text="Token 验证失败，请重新输入。")
         except Exception as e:
-            status_label.config(text=f"验证错误: {e}")
+            error_message = str(e)
+            if "443" in error_message:
+                status_label.config(text="验证错误，你好像没关梯子")
+            else:
+                status_label.config(text=f"验证错误，{e}")
 
     confirm_button = Button(token_window, text="确认", command=validate_token)
     confirm_button.pack()
@@ -218,15 +285,12 @@ def token_input_window():
     def open_link():
         webbrowser.open("https://github.com/Yuchen114514/bupt-classrooom-searcher")
 
-    link_label = Label(
-        token_window, text="如何获取Token？点击访问GitHub查看readme", fg="blue", cursor="hand2"
-    )
+    link_label = Label(token_window, text="如何获取Token？点击", fg="blue", cursor="hand2")
     link_label.pack()
     link_label.bind("<Button-1>", lambda e: open_link())
 
     token_window.mainloop()
 
 
-# 脚本开始
 if __name__ == "__main__":
     token_input_window()
